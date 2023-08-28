172f49def138018d9c621d43ef55d60c86ea2f7b
rafaelSantosSousa <rafael.ti1@outlook.com>
2023-08-28 00:26:22 -0300

    Insert first repository

--- src/main/java/com/br/appliedStudent/models/Person.java
+++ src/main/java/com/br/appliedStudent/models/Person.java
@@ -1,11 +1,24 @@
 package com.br.appliedStudent.models;
 
-public class Person {
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+
+@Entity
+@Table(name="person")
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name",nullable = false, length = 80)
     private String firstName;
+    @Column(name = "last_name",nullable = false, length = 80)
     private String lastName;
+    @Column(nullable = false, length = 100)
     private String Address;
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person(){
