723ea1d48cd9020ad0ecc1cc7b92d575cdba4534
rafaelSantosSousa <rafael.ti1@outlook.com>
2023-09-11 03:15:43 -0300

    update

--- src/main/java/com/br/appliedStudent/data/vo/vo1/PersonVO.java
+++ src/main/java/com/br/appliedStudent/data/vo/vo1/PersonVO.java
@@ -1,5 +1,7 @@
 package com.br.appliedStudent.data.vo.vo1;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 
 import java.io.Serializable;
@@ -8,12 +10,17 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+//Anotação @JsonPropertyOrder serve para alterar ordem dos atributos nas requisições
+//alterar o nome da requisição das colunas é a Anotação @JsonProperty
+@JsonPropertyOrder({"id","address","lastName","firstName","gender"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String Address;
     private String gender;
